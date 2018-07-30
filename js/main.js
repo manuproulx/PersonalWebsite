95aaa81ccd16cfeb3cd171642d02b0ca35344676
Emmanuel Proulx <foufoulefou2juillet@hotmail.com>
2018-07-30 15:40:02 -0400

    Changed typewriter function so it can be dynamically called from any other Javascript code

--- js/main.js
+++ js/main.js
@@ -21,5 +21,21 @@ new fullpage('#fullpage', {
 //methods
 fullpage_api.setAllowScrolling(true);
 
+new Typewriter(125, document.getElementById("typedtext"), "<I like to build great things/>", "appendDownArrow()")
 
-
+function appendDownArrow(){
+    var button = document.createElement("div")
+    button.setAttribute("id", "arrowDownDiv")
+    button.onclick = function(){
+        fullpage_api.moveSectionDown()
+    }
+    document.getElementById("selectionDiv").appendChild(button)
+    var text = document.createElement("span")
+    text.innerHTML = "Take the tour"
+    var icon = document.createElement("i")
+    icon.setAttribute("class", "material-icons")
+    icon.setAttribute("id", "arrowDown")
+    icon.innerHTML = "keyboard_arrow_down"
+    document.getElementById("arrowDownDiv").appendChild(text)
+    document.getElementById("arrowDownDiv").appendChild(icon)
+}
--- js/typewriter.js
+++ js/typewriter.js
@@ -1,66 +1,33 @@
-// set up text to print, each item in array is new line
-var aText = new Array(
-    "<I like to build great things/>");
-var iSpeed = 125; // time delay of print out
-var iIndex = 0; // start printing array at this posision
-var iArrLength = aText[0].length; // the length of the text array
-var iScrollAt = 20; // start scrolling up at this many lines
-
-var iTextPos = 0; // initialise text position
-var sContents = ''; // initialise contents variable
-var iRow; // initialise current row
-
-function typewriter() {
-    sContents = ' ';
-    iRow = Math.max(0, iIndex - iScrollAt);
-    var destination = document.getElementById("typedtext");
-
-    while (iRow < iIndex) {
-        sContents += aText[iRow++] + '<br />';
-        
-    }
-    destination.innerText = sContents + aText[iIndex].substring(0, iTextPos);
-    if (iTextPos++ == iArrLength) {
-        iTextPos = 0;
-        iIndex++;
-      
-        setTimeout("appendDownArrow()", 1000)
-        if (iIndex != aText.length) {
-            iArrLength = aText[iIndex].length;
-            setTimeout("typewriter()", 500);
-        
-           
-
-        }
-    } else {
-
-        setTimeout("typewriter()", iSpeed);1
-  
-        
-    }
-}
-
-setTimeout("typewriter()", 1000)
-
-function appendDownArrow(){
-    var button = document.createElement("div")
-    button.setAttribute("id", "arrowDownDiv")
-    button.onclick = function(){
-        fullpage_api.moveSectionDown()
-    }
-    document.getElementById("selectionDiv").appendChild(button)
-    var text = document.createElement("span")
-    text.innerHTML = "Take the tour"
-    var icon = document.createElement("i")
-    icon.setAttribute("class", "material-icons")
-    icon.setAttribute("id", "arrowDown")
-    icon.innerHTML = "keyboard_arrow_down"
-    document.getElementById("arrowDownDiv").appendChild(text)
-    document.getElementById("arrowDownDiv").appendChild(icon)
-   /* $('.text').html(function(i, html) {
-        var chars = $.trim(html).split("");
-      
-        return '<span>' + chars.join('</span><span>') + '</span>';
-      });*/
+/**
+ * A typewriter that dynamically emulates the input of a user
+ * @param {*} iterationSpeed The speed at which the text is typed
+ * @param {*} destinationElement The HTML element in which the inner text will be inserted
+ * @param {*} inputText The text to type
+ * @param {*} callback (Optional) The function to execute once the typing is done
+ */
+var Typewriter = function(iterationSpeed, destinationElement, inputText,callback){
+    iterateTypewriter = function(iterationSpeed,destinationElement,callback) {
+        return function(inputText,iTextPos){
+            
+            iArrLength = inputText.length
+            
+            
     
+            
+            destinationElement.innerText = inputText.substring(0, iTextPos);
+            if (iTextPos++ == iArrLength) {
+    
+                setTimeout(callback, 1000)
+                
+            } else {
+                
+                setTimeout("iterateTypewriter(\"" + inputText.replace("\"", "") + "\"," + iTextPos  +  ")", iterationSpeed);
+        
+                
+            }
+        }
+        
+    }(iterationSpeed,destinationElement,callback)
+    
+    setTimeout("iterateTypewriter(\"" + inputText.replace("\"", "") + "\"," + 0  +  ")", 1000);
 }
