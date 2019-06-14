c9c13ef70db7ed1e1b4ef6bd5cfc2f2c4c172d64
root <root@DESKTOP-VVRL69J.localdomain>
2019-06-14 16:20:19 -0400

    Add personal projects and extra project card into component

--- personalwebsite/src/app/Projects/ProjectsGrid/ProjectsGrid.js
+++ personalwebsite/src/app/Projects/ProjectsGrid/ProjectsGrid.js
@@ -1,28 +1,59 @@
 import React from 'react';
-import { Card, Image, Header, Icon } from 'semantic-ui-react';
+import { Card, Header, Icon, Responsive } from 'semantic-ui-react';
 import './ProjectsGrid.scss';
+import ProjectCard from './ProjectCard/ProjectCard';
+
+const projects = [
+    {
+        name: 'ÉTSMobile',
+        platform: 'iOS (Swift) and Android (Kotlin) app',
+        description: 'Open-source mobile application available to all university students to check their grades, schedule, read school news, etc.',
+        image: 'etsmobile.PNG',
+        github: 'https://github.com/ApplETS/Notre-Dame'
+    },
+    {
+        name: 'ApplETS club website',
+        platform: 'Static front-end website hosted on Netlify',
+        description: 'Open-source website for a mobile development student club. The students can discover our projects and apply to join the club through the website.',
+        github: 'https://github.com/ApplETS/Site-Web-App-ETS',
+        image: 'clubapplets.png',
+        website: 'https://clubapplets.ca/'
+    },
+    {
+        name: 'GuideFinder',
+        platform: 'Multiplatform mobile app made with Flutter',
+        description: 'Proof of concept app made during StarterHacks 2018 at UWaterloo. Lets the user look for guides at specific locations, and plan a trip with them. Aims to connect the locals with tourists - to humanize travelling.',
+        image: 'travelguide.png',
+        github: 'https://github.com/GuideFinder/guide_finder_app',
+        website: 'https://guidefinder.github.io/'
+    },
+    {
+        name: 'Portfolio website',
+        platform: 'React app with static content, GitHub GraphQL API fetching',
+        description: 'Small single-page portfolio website for presentation of personal projects.',
+        image: 'portfoliowebsite.jpg',
+        website: 'https://emmanuelproulx.com'
+    }
+]
 
 const ProjectsGrid = () => (
     <div className="ProjectsGrid" >
-        <Header style={{ paddingTop: '50px' }} as="h1">Projects <Icon name="book" /></Header>
-        <Card.Group style={{ display: 'flex', justifyContent: 'center'}}>
-            <Card>
-                <Image src={require('../../../assets/etsmobile.PNG')} />
-                <Card.Content>
-                    <Card.Header>Project name</Card.Header>
-                    <Card.Meta>Project language</Card.Meta>
-                    <Card.Description>
-                        Project description
-                    </Card.Description>
-                </Card.Content>
-                <Card.Content >
-                    <a href="/">
-                        <Icon name="github"/>
-                        Project repository
-                    </a>
-                </Card.Content>
-            </Card>
-        </Card.Group>
+        <Header as="h1">Projects <Icon name="book" /></Header>
+        <Responsive maxWidth={993}>
+            <Card.Group style={{ display: 'flex', justifyContent: 'center'}}>
+                {projects.map(project => (
+                    <ProjectCard {...project} />
+                ))}
+            </Card.Group>
+        </Responsive>
+        <Responsive minWidth={994}>
+            <Card.Group itemsPerRow={2} style={{ display: 'flex', justifyContent: 'center'}}>
+                {projects.map(project => (
+                    <ProjectCard {...project} />
+                ))}
+            </Card.Group>
+        </Responsive>
+
     </div>
 );
 
