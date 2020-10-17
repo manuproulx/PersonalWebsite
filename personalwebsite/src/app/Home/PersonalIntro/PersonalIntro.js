9528ab66b9c92a795631de26c9a5f8ea45f9786a
Emmanuel Proulx <emmanuel.proulx@hotmail.com>
2020-10-17 01:15:19 -0400

    Remove signature

--- personalwebsite/src/app/Home/PersonalIntro/PersonalIntro.js
+++ personalwebsite/src/app/Home/PersonalIntro/PersonalIntro.js
@@ -13,7 +13,6 @@ class PersonalIntro extends React.Component {
                     <div className="PersonalIntro__Card__Text">
                         <Header as="h1">Nice to meet you.</Header>
                         <p>I'm a 22 year-old undergraduate software engineering student from Montréal, Canada. I share a passion, amongst other things, for mobile and web development. You'll find on this website some of the work I had the chance to do in the past years, with a bit more insight on my professional experience. Feel free to reach out!</p>
-                        <Image style={{ height: '80px' }} src={require('../../../assets/ep-signature.png')} />
                     </div>
 
                 </div>
